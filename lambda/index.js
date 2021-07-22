60675668355b78c9bd5eab3e44f3eee10fd2ae79
Danilotve <fsodanilo@gmail.com>
2021-07-21 23:34:02 -0300

    novo bem vindo

--- lambda/index.js
+++ lambda/index.js
@@ -8,7 +8,7 @@ const LaunchRequestHandler = {
         return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
     },
     handle(handlerInput) {
-        const speakOutput = 'bem vindo, you can say Hello or Help. Which would you like to try?';
+        const speakOutput = 'bem vindo, deseja meditar?';
         return handlerInput.responseBuilder
             .speak(speakOutput)
             .reprompt(speakOutput)
